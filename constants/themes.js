82b0be9767b4d87804e63bd7cf1eaa1b7f1bfaaa
Alex-Au <AlexXianZhenYuAu@gmail.com>
2024-09-25 13:38:35 -0400

    Added header and footer
    - Also started a bit on the look for the menu
    - Theme selection on header does not work yet

---
+++ constants/themes.js
@@ -0,0 +1,21 @@
+export const ThemeNames = {
+    Light: "light",
+    Dark: "dark",
+    Blue: "blue"
+}
+
+export const DefaultTheme = ThemeNames.Light;
+
+export const Themes = {};
+
+Themes[ThemeNames.Light] = {
+    fontColour: "#333333",
+    primary: "#26374a",
+    primaryBg: "#ffffff",
+    primaryHover: "#444444",
+    secondary: "#335075",
+    secondaryBg: "#cfd1d5",
+    tertiary: "#af3c43",
+    link: "#284162",
+    headerTitleColor: "#000000"
+};
